ca3629c6ba8604161db3ec3e78557ed3f71ee49a
budin <syihabupnyk@gmail.com[200~git config --global user.email syihabupnyk@gmail.com>
2022-09-26 19:56:43 +0700

    landingpage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Landingpage from './components/landingpage';
+
+function App() {
+  return (
+<>
+    <Navbar/>
+    <Landingpage/>
+  </>
+  );
+}
+
+export default App;
